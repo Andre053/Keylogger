1bdcd709a76709786f9e3b0a922c39e6727623a8
Andre053 <andre.debiasi053@gmail.com>
2022-05-01 19:56:54 -0400

    Added logger header and code files

---
+++ Code Files/logger.cpp
@@ -0,0 +1,16 @@
+#include "../Header Files/logger.hpp"
+
+void Log::listen() {
+    std::cout << "Starting to listen" << std::endl;
+
+}
+
+void Output::save_to_file() {
+    std::cout << "Save current logs to a file" << std::endl;
+
+}
+
+void Output::email_logs() {
+    std::cout << "Email current logs to an address" << std::endl;
+    
+}
---
+++ Header Files/logger.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Log {
+    public:
+        Log();
+        void listen();
+
+    private:
+};
+
+class Output {
+    public:
+        Output();
+        void save_to_file();
+        void email_logs();
+
+};
